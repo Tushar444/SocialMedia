079671bce8a6b68afcf8d6d9e86d2dba171903a9
Tushar Mundhra <tusharmundhra444@gmail.com>
2024-07-17 01:37:02 +0530

    add secure: true for sending cookies over https

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -53,6 +53,7 @@ export const login = (req, res) => {
     res
       .cookie("accessToken", token, {
         httpOnly: false,
+        secure: true,
       })
       .status(200)
       .json(others);
